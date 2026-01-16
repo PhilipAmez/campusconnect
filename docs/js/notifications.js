ffbd07f85dac86f612980baae2dbd195d04476df
Philipamez <philipamezuweh@gmail.com>
2026-01-16 20:22:36 +0000

    notifi i

--- docs/js/notifications.js
+++ docs/js/notifications.js
@@ -4,33 +4,33 @@ import { supabase } from './supabaseClient.js';
 // ============= NOTIFICATION HELPER FUNCTIONS =============
 function playNotificationSound() {
     try {
-        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        const oscillator = audioContext.createOscillator();
-        const gainNode = audioContext.createGain();
-        
-        oscillator.connect(gainNode);
-        gainNode.connect(audioContext.destination);
-        
-        oscillator.frequency.value = 800;
-        oscillator.type = 'sine';
-        
-        gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
-        gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.5);
-        
-        oscillator.start(audioContext.currentTime);
-        oscillator.stop(audioContext.currentTime + 0.5);
+        const audio = new Audio('/sounds/notification.mp3');
+        audio.volume = 0.5;
+        audio.play().catch(error => console.log('Could not play notification sound:', error));
     } catch (error) {
         console.log('Notification sound error:', error);
     }
 }
 
 function showDesktopNotification(title, options = {}) {
-    if ('Notification' in window && Notification.permission === 'granted') {
-        new Notification(title, {
-            icon: '/android-chrome-192x192.png',
-            badge: '/android-chrome-192x192.png',
-            ...options
-        });
+    if ('Notification' in window) {
+        if (Notification.permission === 'granted') {
+            new Notification(title, {
+                icon: '/android-chrome-192x192.png',
+                badge: '/android-chrome-192x192.png',
+                ...options
+            });
+        } else if (Notification.permission === 'default') {
+            Notification.requestPermission().then(permission => {
+                if (permission === 'granted') {
+                    new Notification(title, {
+                        icon: '/android-chrome-192x192.png',
+                        badge: '/android-chrome-192x192.png',
+                        ...options
+                    });
+                }
+            });
+        }
     }
 }
 
